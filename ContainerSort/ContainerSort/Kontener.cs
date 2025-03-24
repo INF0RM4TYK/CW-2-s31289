cf37e2c847a833f52de79a013c7eff23f8d4cf57
mateuszgorka <gorkamateusz405@gmail.com>
2025-03-24 13:00:01 +0100

    New Data

---
+++ ContainerSort/ContainerSort/Kontener.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace ContainerSort;
+
+public class Kontener
+{
+    
+    public double Masa {get; set;}
+    public double Wyskosc {get; set;}
+    public double WagaWlasna {get; set;}
+    public double Glebokosc {get; set;} 
+    public string NumerSeryjny {get; set;}
+    public double MaxLadownosc {get; set;} 
+    
+    
+    
+    public Kontener(double masa, double wyskosc, double wagaWlasna, double glebokosc, string numerSeryjny, double maxLadownosc)
+    { 
+        Masa = masa;
+        Wyskosc = wyskosc;
+        WagaWlasna = wagaWlasna;
+        Glebokosc = glebokosc;   
+        NumerSeryjny = numerSeryjny;
+        MaxLadownosc = maxLadownosc;
+    }
+
+    
+    
+    
+    
+
+}
